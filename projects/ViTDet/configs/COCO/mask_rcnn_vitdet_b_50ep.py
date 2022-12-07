7e08b8054514fd3d588427e25b13f9f59bfcc500
YuanLiuuuuuu <3463423099@qq.com>
2022-12-07 19:21:58 +0800

    [Feature]: Add 50e pretrain for ViTDet

---
+++ projects/ViTDet/configs/COCO/mask_rcnn_vitdet_b_50ep.py
@@ -0,0 +1,55 @@
+from functools import partial
+from fvcore.common.param_scheduler import MultiStepParamScheduler
+
+from detectron2 import model_zoo
+from detectron2.config import LazyCall as L
+from detectron2.solver import WarmupParamScheduler
+from detectron2.modeling.backbone.vit import get_vit_lr_decay_rate
+
+from ..common.coco_loader_lsj import dataloader
+
+model = model_zoo.get_config("common/models/mask_rcnn_vitdet.py").model
+
+# Initialization and trainer settings
+train = model_zoo.get_config("common/train.py").train
+train.amp.enabled = True
+train.ddp.fp16_compression = True
+train.init_checkpoint = "/mnt/cache/liuyuan/research/mae/pretrain_s2/mae_only_low_r40_src_single_1600e/checkpoint-1599_.pth"
+train.output_dir = "work_dir/mae_det/mae_jitter_gray_target_300e"
+
+# Schedule
+# 100 ep = 184375 iters * 64 images/iter / 118000 images/ep
+train.max_iter = 184375 // 2
+train.checkpointer.max_to_keep = 1
+
+lr_multiplier = L(WarmupParamScheduler)(
+    scheduler=L(MultiStepParamScheduler)(
+        values=[1.0, 0.1, 0.01],
+        milestones=[163889 // 2, 177546 // 2],
+        num_updates=train.max_iter,
+    ),
+    warmup_length=250 / train.max_iter,
+    warmup_factor=0.001,
+)
+
+# Optimizer
+optimizer = model_zoo.get_config("common/optim.py").AdamW
+optimizer.params.lr_factor_func = partial(
+    get_vit_lr_decay_rate, num_layers=12, lr_decay_rate=0.75)
+optimizer.params.overrides = {"pos_embed": {"weight_decay": 0.0}}
+
+# ceph
+file_client_args = dict(
+    backend='petrel',
+    path_mapping=dict({
+        'datasets/': 's3://openmmlab/datasets/detection/',
+        '.datasets/': 's3://openmmlab/datasets/detection/'
+    }))
+
+# If you don’t need ceph, you can directly comment the following code or
+# set file_client_args to None
+dataloader.train.mapper.file_client_args = file_client_args
+dataloader.test.mapper.file_client_args = file_client_args
+
+dataloader.train.num_workers = 16
+dataloader.test.num_workers = 16
